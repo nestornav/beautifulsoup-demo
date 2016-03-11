73c3be8ce8d523730c6150aa1aa000040c0c4567
Nestor <nestornav@gmail.com>
2016-03-10 23:40:09 -0300

    Dump the data of the farmacy

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+import requests as req
 from bs4 import BeautifulSoup as bs
 
-
 def run():
-	file2read = open("farm-turno")
-	soup = bs(file2read,"html.parser")
-	file2read.close()
-	print( soup.prettify() )
+	
+	#I will get all the all-night farmacy
+	file2read = req.get( "http://www.colfacor.org.ar/turnosfarmacias/grid_turno_farmacia/" )	
+	soup = bs(file2read.content,"html.parser")	
+	
+	#create a file where I will dump the data extracted	for test pourpuse
+	file2write = open("table-data","w")
+
+	#Get the data in an ugly way
+	for tag in soup.find_all('table'):		
+		table_raw = tag.attrs
+		table_id = table_raw.get('id')		
+		if str(table_id).find("#") != -1:
+			file2write.write( str(tag) )						
+	file2write.close()
 
 if __name__ == '__main__':
 	run()
